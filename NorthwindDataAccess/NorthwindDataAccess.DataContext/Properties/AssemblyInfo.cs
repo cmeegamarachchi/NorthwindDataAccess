8b63ba6fe41814d7e3464a3a0e0dc4b304f5cddc
Chintana Meegamarachchi <chintana@appsolve.com.au>
2016-01-29 11:59:25 +0800

    Updates assembly details

--- NorthwindDataAccess/NorthwindDataAccess.DataContext/Properties/AssemblyInfo.cs
+++ NorthwindDataAccess/NorthwindDataAccess.DataContext/Properties/AssemblyInfo.cs
@@ -1,25 +1,27 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("NorthwindDataAccess.DataContext")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit of work based interface to Northwind Database")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("App Solve Pvt Ltd")]
 [assembly: AssemblyProduct("NorthwindDataAccess.DataContext")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © App Solve Pvt Ltd 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("eaf39021-1fac-4809-95d6-6c67bd1301ee")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- NorthwindDataAccess/NorthwindDataAccess.Ef/Properties/AssemblyInfo.cs
+++ NorthwindDataAccess/NorthwindDataAccess.Ef/Properties/AssemblyInfo.cs
@@ -1,25 +1,27 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("NorthwindDataAccess.Ef")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Entity framework based implementation for IRepository interface")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("App Solve Pvt Ltd")]
 [assembly: AssemblyProduct("NorthwindDataAccess.Ef")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © App Solve Pvt Ltd 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("503358df-3e58-4dcc-9943-31cfdbad984c")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
